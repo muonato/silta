65fba4c5a3d3700df3d1d7da270ac644fb82b0fd
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-02-16 12:18:49 +0200

    Add files via upload

---
+++ silta/utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+#  SIMPLE LOCALIZED TASKS
+# v1.0 github/muonato 2022
+
+import json
+
+ATTR_FILE = "data/silta.json"
+
+def load_attr(lang_file):
+    try:
+        with open(lang_file) as f:
+            lang = json.load(f)
+            
+    except FileNotFoundError:
+        lang = []
+
+    with open(ATTR_FILE) as f:
+        attr = json.load(f)
+
+    for i, d in enumerate(lang):
+        for key in ["HTM_COMMON", "HTM_OBJECT", "HTM_NAVDIV"]:
+            if key in d:
+                attr[i][key].update(d[key])
+    return attr
