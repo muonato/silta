1f4e2420b3e78475f62ee2f3d51ce93f4c3569b1
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-04-25 06:34:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-#
-# SIMPLE LOCALIZED TASKS
-# Copyright 2022 muonato
-#   GNU License GPL v3
+# github/muonato/silta
 
+"""Silta application launcher.
+
+Args:
+    -a --address
+    -p --port
+    -d --database
+    -t --theme
+    -h --help
+"""
 import argparse
 import socketserver
 
-from silta.facde import Silta
+from silta.logic import Silta
 from silta.shttp import Handler
 
 def main():
-    print(f"\n\t\tSILTA v1.0 Copyright 2022 muonato"\
+    print(f"\n\t\tSILTA v1.0 Copyright 2022 -muonato-"\
             "\nReleased under the terms of "\
             "the GNU General Public License GPLv3\n")
 
@@ -26,18 +32,24 @@ def main():
                     help="server HTTP service port, default: 8000")
     options.add_argument("-d", "--database", default="data/silta.db",
                     help="sqlite database file")
-    options.add_argument("-t", "--theme", default="ui/default",
-                    help="browser theme folder, default: 'ui/default'")
-    options.add_argument("-m", "--mysql", default=None,
-                    help="mysql db, example: 'user:pass@address:port:database'")
+    options.add_argument("-t", "--theme", default="ui/silta",
+                    help="browser theme path, default: 'ui/silta'")
 
     args = options.parse_args()
 
-    Handler.silta = Silta(args.database, args.theme, args.mysql)
+    Handler.silta = Silta(args.database, args.theme)
 
+    socketserver.TCPServer.allow_reuse_address = True
     with socketserver.TCPServer(
             (args.address, int(args.port)), Handler) as httpd:
-                httpd.serve_forever()
+                try:
+                    print(f"Socketserver {args.address}:{args.port} ")
+                    httpd.serve_forever()
+
+                except KeyboardInterrupt:
+                    print("\nSocketserver shutdown.")
+                    httpd.shutdown()
+                    httpd.socket.close()
 
 if __name__ == "__main__":
         main()
