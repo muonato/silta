52b9d50640b7afe6102bf730a04bc7b60922ad2b
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-04-28 14:19:26 +0300

    Add files via upload

--- data/silta.db.sql
+++ data/silta.db.sql
@@ -4,16 +4,16 @@ CREATE TABLE IF NOT EXISTS "tasks" (
 	"task_host"	INTEGER,
 	"task_done"	INTEGER DEFAULT 0,
 	"task_name"	TEXT DEFAULT '(new)',
-	FOREIGN KEY("task_host") REFERENCES "tasks"("task_id"),
-	PRIMARY KEY("task_id" AUTOINCREMENT)
+	PRIMARY KEY("task_id" AUTOINCREMENT),
+	FOREIGN KEY("task_host") REFERENCES "tasks"("task_id")
 );
 CREATE TABLE IF NOT EXISTS "attrs" (
 	"attr_id"	INTEGER NOT NULL UNIQUE,
 	"attr_host"	INTEGER,
 	"attr_type"	TEXT,
-	"attr_name"	TEXT DEFAULT '(new)',
+	"attr_name"	TEXT DEFAULT 'Unknown',
 	"attr_data"	TEXT DEFAULT '(new)',
-	PRIMARY KEY("attr_id" AUTOINCREMENT),
-	FOREIGN KEY("attr_host") REFERENCES "tasks"("task_id")
+	FOREIGN KEY("attr_host") REFERENCES "tasks"("task_id"),
+	PRIMARY KEY("attr_id" AUTOINCREMENT)
 );
 COMMIT;
